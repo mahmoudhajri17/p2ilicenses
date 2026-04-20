20df4b6e4c34194c8907a971df37897d877a07d9
mahmoudhajri17 <mahmoud.hajri@zum-it.com>
2026-04-20 15:38:13 +0100

    aaa

--- backend/settings.py
+++ backend/settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = 'django-insecure-5)wby_mq8s!l^$$jnt&kas6flarl)^&41&1h1$x7bmqh)x@=@b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
