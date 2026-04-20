423ce80014f17ba0bc62793c1462f8e58dde865a
mahmoudhajri17 <mahmoud.hajri@zum-it.com>
2026-04-20 16:14:02 +0100

    aaa

--- backend/settings.py
+++ backend/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5)wby_mq8s!l^$$jnt&kas6flarl)^&41&1h1$x7bmqh)x@=@b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
